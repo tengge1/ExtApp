e345509741da0a8e8e53f3be7f6baefd66f81388
Li Teng <930372551@qq.com>
2017-03-02 07:54:00 +0800

    大小写修改

---
+++ ExtApp/ExtApp.Web/app/view/sys/appMenu/Add.js
@@ -0,0 +1,112 @@
+﻿/**
+* 添加菜单
+*/
+
+Ext.define('App.view.sys.appMenu.Add', {
+    extend: 'Ext.window.Window',
+    alias: 'widget.appmenuadd',
+
+    requires: [
+        'App.view.sys.appMenu.AddController'
+    ],
+
+    controller: 'appmenuadd',
+
+    title: '添加菜单',
+    width: 500,
+    layout: 'fit',
+    modal: true,
+    iconCls: 'Applicationsidetree',
+
+    items: {
+        xtype: 'form',
+        border: false,
+        layout: 'column',
+        defaults: {
+            margin: 10,
+            columnWidth: 0.5
+        },
+        fieldDefaults: {
+            labelAlign: 'right',
+            labelWidth: 60
+        },
+        items: [{
+            xtype: 'hiddenfield',
+            name: 'PID'
+        }, {
+            xtype: 'textfield',
+            name: 'PName',
+            fieldLabel: '上级菜单',
+            readOnly: true
+        }, {
+            xtype: 'textfield',
+            name: 'Name',
+            fieldLabel: '菜单名称',
+            allowBlank: false
+        }, {
+            xtype: 'combo',
+            store: [[
+                0, '未设置'
+            ], [
+                1, 'ExtJs类名'
+            ], [
+                2, 'Url地址'
+            ]],
+            name: 'UrlType',
+            value: 0,
+            editable: false,
+            fieldLabel: '菜单类型'
+        }, {
+            xtype: 'textfield',
+            name: 'Url',
+            fieldLabel: '类名/Url'
+        }, {
+            xtype: 'combo',
+            store: [[
+                0, '未设置'
+            ], [
+                1, 'css样式'
+            ], [
+                2, '图片url'
+            ]],
+            name: 'IconType',
+            value: 0,
+            editable: false,
+            fieldLabel: '图标类型'
+        }, {
+            xtype: 'textfield',
+            name: 'Icon',
+            fieldLabel: '图标'
+        }, {
+            xtype: 'numberfield',
+            name: 'Layer',
+            fieldLabel: '菜单排序',
+            allowBlank: false,
+            value: 0
+        }, {
+            xtype: 'combo',
+            store: [[
+                0, '启用'
+            ], [
+                -1, '禁用'
+            ]],
+            name: 'Status',
+            value: 0,
+            editable: false,
+            fieldLabel: '状态'
+        }]
+    },
+    buttons: [{
+        text: '保存',
+        iconCls: 'Accept',
+        listeners: {
+            click: 'onSaveClick'
+        }
+    }, {
+        text: '取消',
+        iconCls: 'Cancel',
+        listeners: {
+            click: 'onCancelClick'
+        }
+    }]
+});

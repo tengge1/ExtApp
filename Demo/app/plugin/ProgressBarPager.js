2cbb99fd3330c05221ebb155fbf4dce134d838d2
Li Teng <930372551@qq.com>
2017-03-01 12:14:12 +0800

    大小写修改

---
+++ Demo/app/plugin/ProgressBarPager.js
@@ -0,0 +1,10 @@
+﻿/*
+* 数据表数据加载进度插件
+*/
+
+Ext.define('App.plugin.ProgressBarPager', {
+    extend: 'Ext.ux.ProgressBarPager',
+    alias: 'plugin.progressbarpager',
+
+    defaultText: '加载中...'
+});

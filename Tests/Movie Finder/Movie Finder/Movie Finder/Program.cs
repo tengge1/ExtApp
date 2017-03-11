cb411b62fd4072dbc993db387aafc8586d7303c4
Li Teng <930372551@qq.com>
2017-03-11 21:40:35 +0800

    测试

---
+++ Tests/Movie Finder/Movie Finder/Movie Finder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SpringNet;
+
+namespace Movie_Finder
+{
+    /// <summary>
+    /// 电影应用
+    /// </summary>
+    public class MovieApp
+    {
+        public static void Main(string[] args)
+        {
+            IApplicationContext ctx = ContextRegistry.GetContext();
+        }
+    }
+}

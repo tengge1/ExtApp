c3faa3d8c6d40701062c200fb3fd610a11bab830
Li Teng <930372551@qq.com>
2017-03-12 11:23:02 +0800

    Movie Finder+log4net测试

---
+++ Tests/Movie Finder/Movie Finder/Movie Finder/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie_Finder
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+
+        public string Director { get; set; }
+    }
+}
---
+++ Tests/Movie Finder/Movie Finder/Movie Finder/MovieLister.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie_Finder
+{
+    public class MovieLister
+    {
+        public IMovieFinder movieFinder { get; set; }
+
+        public Movie[] MoviesDirectedBy(string director)
+        {
+            return movieFinder.FindMovie().Where(o => o.Director == director).ToArray();
+        }
+    }
+}

61126ed65056cb3b54a2575171b3487026cf7b37
Li Teng <930372551@qq.com>
2017-04-26 12:14:09 +0800

    工作流设计

--- ExtApp/ExtApp.Web/app/view/main/accordion/MenuController.js
+++ ExtApp/ExtApp.Web/app/view/main/accordion/MenuController.js
@@ -78,51 +78,83 @@ Ext.define('App.view.main.accordion.MenuController', {
         }
         App.query('viewport').mask('请稍后...');
         var openType = record.data.OpenTypeCode;
-        if (openType == 'NoIframe' && record.data.UrlTypeCode != 'ExtJsClass') { // 除ExtJs类外，其他url不允许以NoIframe方式打开
-            openType = 'Iframe';
-        }
-        switch (openType) { // 打开方式
-            case 'NoIframe': // 非iframe方式
-                try {
-                    p = Ext.create(url);
-                } catch (e) {
+        if (record.data.UrlTypeCode == 'ExtJsClass') { // ExtJs类
+            switch (openType) { // 打开方式
+                case 'NoIframe': // 非iframe方式
+                    try {
+                        p = Ext.create(url);
+                    } catch (e) {
+                        App.query('viewport').unmask();
+                        App.notify('消息', '该URL不存在！');
+                        return;
+                    }
+                    tp.add(p);
+                    tp.setActiveTab(p);
                     App.query('viewport').unmask();
-                    App.notify('消息', '该URL不存在！');
-                    return;
-                }
-                tp.add(p);
-                tp.setActiveTab(p);
-                App.query('viewport').unmask();
-                break;
-            case 'Iframe': // iframe方式
-                p = Ext.create('Ext.Container', {
-                    title: record.data.Name,
-                    closable: true,
-                    html: '<iframe src="iframe.html?cls=' + url + '" width="100%" height="100%" frameborder="0" scrolling="auto" onload="App.query(\'viewport\').unmask();"></iframe>'
-                });
-                tp.add(p);
-                tp.setActiveTab(p);
-                break;
-            case 'Dialog': // 对话框方式
-                p = Ext.create('Ext.window.Window', {
-                    title: record.data.Name,
-                    width: '90%',
-                    height: '90%',
-                    closable: true,
-                    modal: true,
-                    html: '<iframe src="iframe.html?cls=' + url + '" width="100%" height="100%" frameborder="0" scrolling="auto" onload="App.query(\'viewport\').unmask();"></iframe>'
-                });
-                p.show();
-                App.query('viewport').unmask();
-                break;
-            case 'Top': // 顶层窗口打开
-                App.query('viewport').unmask();
-                top.window.location = 'iframe.html?cls=' + url;
-                break;
-            case 'Blank': // 新选项卡
-                App.query('viewport').unmask();
-                window.open('iframe.html?cls=' + url, '_blank');
-                break;
+                    break;
+                case 'Iframe': // iframe方式
+                    p = Ext.create('Ext.Container', {
+                        title: record.data.Name,
+                        closable: true,
+                        html: '<iframe src="iframe.html?cls=' + url + '" width="100%" height="100%" frameborder="0" scrolling="auto" onload="App.query(\'viewport\').unmask();"></iframe>'
+                    });
+                    tp.add(p);
+                    tp.setActiveTab(p);
+                    break;
+                case 'Dialog': // 对话框方式
+                    p = Ext.create('Ext.window.Window', {
+                        title: record.data.Name,
+                        width: '90%',
+                        height: '90%',
+                        closable: true,
+                        modal: true,
+                        html: '<iframe src="iframe.html?cls=' + url + '" width="100%" height="100%" frameborder="0" scrolling="auto" onload="App.query(\'viewport\').unmask();"></iframe>'
+                    });
+                    p.show();
+                    App.query('viewport').unmask();
+                    break;
+                case 'Top': // 顶层窗口打开
+                    App.query('viewport').unmask();
+                    top.window.location = 'iframe.html?cls=' + url;
+                    break;
+                case 'Blank': // 新选项卡
+                    App.query('viewport').unmask();
+                    window.open('iframe.html?cls=' + url, '_blank');
+                    break;
+            }
+        } else { // 普通url
+            switch (openType) { // 打开方式
+                case 'NoIframe': // 非iframe方式
+                case 'Iframe': // iframe方式
+                    p = Ext.create('Ext.Container', {
+                        title: record.data.Name,
+                        closable: true,
+                        html: '<iframe src="' + url + '" width="100%" height="100%" frameborder="0" scrolling="auto" onload="App.query(\'viewport\').unmask();"></iframe>'
+                    });
+                    tp.add(p);
+                    tp.setActiveTab(p);
+                    break;
+                case 'Dialog': // 对话框方式
+                    p = Ext.create('Ext.window.Window', {
+                        title: record.data.Name,
+                        width: '90%',
+                        height: '90%',
+                        closable: true,
+                        modal: true,
+                        html: '<iframe src="' + url + '" width="100%" height="100%" frameborder="0" scrolling="auto" onload="App.query(\'viewport\').unmask();"></iframe>'
+                    });
+                    p.show();
+                    App.query('viewport').unmask();
+                    break;
+                case 'Top': // 顶层窗口打开
+                    App.query('viewport').unmask();
+                    top.window.location = url;
+                    break;
+                case 'Blank': // 新选项卡
+                    App.query('viewport').unmask();
+                    window.open(url, '_blank');
+                    break;
+            }
         }
     }
 });

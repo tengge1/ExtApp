5a63d7d65693f1097def772d69163ee43549ad49
Li Teng <930372551@qq.com>
2017-04-30 17:11:35 +0800

    更新readme文件

--- ExtApp/ExtApp.Web/app/view/main/accordion/Home.js
+++ ExtApp/ExtApp.Web/app/view/main/accordion/Home.js
@@ -17,43 +17,7 @@ Ext.define('App.view.main.accordion.Home', {
     }, {
         xtype: 'displayfield',
         fieldLabel: '版本',
-        value: 'v1.0.1',
-        labelAlign: 'right',
-        labelWidth: 60
-    }, {
-        xtype: 'displayfield',
-        fieldLabel: '作者',
-        value: 'tengge',
-        labelAlign: 'right',
-        labelWidth: 60
-    }, {
-        xtype: 'fieldcontainer',
-        fieldLabel: '特点',
-        items: [{
-            xtype: 'displayfield',
-            hideLabel: true,
-            value: '1、使用NHibernate自动创建数据表。'
-        }, {
-            xtype: 'displayfield',
-            hideLabel: true,
-            value: '2、使用NHibernate Attribute通过属性配置映射，不需要手工编写xml文件。'
-        }, {
-            xtype: 'displayfield',
-            hideLabel: true,
-            value: '3、PC和手机端共享同样的API Controller接口。'
-        }, {
-            xtype: 'displayfield',
-            hideLabel: true,
-            value: '4、ExtJs动态加载js文件。'
-        }, {
-            xtype: 'displayfield',
-            hideLabel: true,
-            value: '5、支持折叠面板和桌面模式。'
-        }, {
-            xtype: 'displayfield',
-            hideLabel: true,
-            value: '6、支持皮肤动态切换。'
-        }],
+        value: 'v1.0.2',
         labelAlign: 'right',
         labelWidth: 60
     }, {

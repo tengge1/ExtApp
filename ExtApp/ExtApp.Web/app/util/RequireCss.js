ebeac6d01a29c89ff3bd801f6c7df5f5a2f15fb4
Li Teng <930372551@qq.com>
2017-05-01 16:15:10 +0800

    CodeMirror代码查看

---
+++ ExtApp/ExtApp.Web/app/util/RequireCss.js
@@ -0,0 +1,26 @@
+﻿
+// ----------- 动态引入css文件 --------------
+
+Ext.define('App.util.RequireCss', {
+    extend: 'Ext.Base',
+
+    require: function (paths) {
+        if (!(paths instanceof Array)) {
+            throw 'paths必须是数组';
+        }
+        for (var i = 0; i < paths.length; i++) {
+            var path = paths[i];
+            var link = Ext.getHead().child('link[name="' + path + '"]');
+            if (link == null) {
+                link = Ext.getHead().appendChild({
+                    tag: 'link',
+                    rel: 'stylesheet',
+                    href: ''
+                });
+                link.set({
+                    href: path
+                });
+            }
+        }
+    }
+});
---
+++ ExtApp/ExtApp.Web/app/util/RequireJs.js
@@ -0,0 +1,25 @@
+﻿
+// ----------- 动态引入js文件 --------------
+
+Ext.define('App.util.RequireJs', {
+    extend: 'Ext.Base',
+
+    require: function (paths) {
+        if (!(paths instanceof Array)) {
+            throw 'paths必须是数组';
+        }
+        for (var i = 0; i < paths.length; i++) {
+            var path = paths[i];
+            var script = Ext.getHead().child('script[name="' + path + '"]');
+            if (script == null) {
+                script = Ext.getHead().appendChild({
+                    tag: 'script',
+                    src: ''
+                });
+                script.set({
+                    src: path
+                });
+            }
+        }
+    }
+});

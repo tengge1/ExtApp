2c822d334d3193f850dcc98686d0f3a01d0cb899
Li Teng <930372551@qq.com>
2017-05-03 07:49:43 +0800

    用户选择控件

--- ExtApp/ExtApp.Web/app/test/UserSelectTest.js
+++ ExtApp/ExtApp.Web/app/test/UserSelectTest.js
@@ -8,7 +8,11 @@ Ext.define('App.test.UserSelectTest', {
     select: null,
 
     tbar: [{
+        xtype: 'hidden',
+        name: 'userIDs'
+    }, {
         xtype: 'textfield',
+        name: 'userNames',
         fieldLabel: '用户',
         labelWidth: 40,
         labelAlign: 'right',
@@ -27,9 +31,18 @@ Ext.define('App.test.UserSelectTest', {
                                 ids += record.data.ID + ',';
                                 names += record.data.Name + ',';
                             }
+                            if (ids.endsWith(',')) {
+                                ids = ids.substr(0, ids.length - 1);
+                            }
+                            if (names.endsWith(',')) {
+                                names = names.substr(0, names.length - 1);
+                            }
+                            sender.up('panel').down('hidden[name=userIDs]').setValue(ids);
+                            sender.up('panel').down('textfield[name=userNames]').setValue(names);
                             App.notify('消息', '您选择了<br />ID:' + ids + '<br />Name:' + names);
                         }
                     });
+                    userSelect.reset();
                     sender.up('panel').select = userSelect;
                     userSelect.show();
                 }
@@ -53,11 +66,9 @@ Ext.define('App.test.UserSelectTest', {
         text: '重置',
         handler: function (sender) {
             var select = sender.up('panel').select;
-            if (select == null) {
-                App.notify('消息', '请选择用户！');
-                return;
-            }
             select.reset();
+            sender.up('panel').down('hidden[name=userIDs]').reset();
+            sender.up('panel').down('textfield[name=userNames]').reset();
         }
     }],
 
@@ -65,28 +76,38 @@ Ext.define('App.test.UserSelectTest', {
         xtype: 'textarea',
         name: 'code1',
         fieldLabel: '控件',
-        value: `var select = Ext.create('App.widget.DeptSelect', {
-            fieldLabel: '机构',
-            labelWidth: 40,
-            labelAlign: 'right',
-            emptyText: '请选择',
-            listeners: {
-            select: function (sender, record, opts) {
-                App.notify('消息', '您选择了<br />ID:' +record.data.ID + '<br />Name:' +record.data.Name);
-            }
-        }
-    }); `
+        value: `var userSelect = Ext.create('App.widget.UserSelect', {
+            callback: function (records) {
+                    var ids = '';
+                    var names = '';
+                    for (var i = 0; i < records.length; i++) {
+                        var record = records[i];
+                        ids += record.data.ID + ',';
+                        names += record.data.Name + ',';
+                    }
+                    if (ids.endsWith(',')) {
+                        ids = ids.substr(0, ids.length -1);
+                    }
+                    if (names.endsWith(',')) {
+                        names = names.substr(0, names.length -1);
+                    }
+                    App.notify('消息', '您选择了<br />ID:' + ids + '<br />Name:' + names);
+                }
+            });
+            userSelect.reset();
+            userSelect.show();`
     }, {
         xtype: 'textarea',
         name: 'code2',
         fieldLabel: '获取值',
-        value: `var value = select.getValue();
-            var text = select.getRawValue();`
+        value: `var value = userSelect.getValue();
+            var text = userSelect.getRawValue();
+            App.notify('消息', '您选择了<br />ID:' + value + '<br />Name:' + text);`
     }, {
         xtype: 'textarea',
         name: 'code3',
         fieldLabel: '重置',
-        value: `select.reset();`
+        value: `userSelect.reset();`
     }],
 
     initComponent: function () {
@@ -101,7 +122,7 @@ Ext.define('App.test.UserSelectTest', {
                 mode: "javascript",
                 readOnly: true
             });
-            editor.setSize('auto', '200px');
+            editor.setSize('auto', '320px');
             editor = CodeMirror.fromTextArea(view.down('textarea[name=code2]').inputEl.dom, {
                 lineNumbers: true,
                 mode: "javascript",

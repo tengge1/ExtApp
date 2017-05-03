a5132b15e3b90519b115fbe1f1ee495e76225373
Li Teng <930372551@qq.com>
2017-05-03 21:52:24 +0800

    画点、画线、画面

---
+++ ExtApp/ExtApp.Web/app/test/MapDrawTest.js
@@ -0,0 +1,129 @@
+﻿
+Ext.define('App.test.MapDrawTest', {
+    extend: 'Ext.panel.Panel',
+
+    layout: 'form',
+    autoScroll: true,
+
+    select: null,
+
+    tbar: [{
+        xtype: 'button',
+        text: '画点',
+        iconCls: 'Map',
+        handler: function () {
+            var win = Ext.create('App.widget.MapDraw', {
+                type: 'point'
+            });
+            win.show();
+        }
+    }, {
+        xtype: 'button',
+        text: '画线',
+        iconCls: 'Map',
+        handler: function () {
+            var win = Ext.create('App.widget.MapDraw', {
+                type: 'polyline'
+            });
+            win.show();
+        }
+    }, {
+        xtype: 'button',
+        text: '画面',
+        iconCls: 'Map',
+        handler: function () {
+            var win = Ext.create('App.widget.MapDraw', {
+                type: 'polygon'
+            });
+            win.show();
+        }
+    }, {
+        xtype: 'button',
+        text: '获取值',
+        handler: function (sender) {
+            var select = sender.up('panel').select;
+            if (select == null) {
+                App.notify('消息', '请选择用户！');
+                return;
+            }
+            var value = select.getValue();
+            var text = select.getRawValue();
+            App.notify('消息', '您选择了<br />ID:' + value + '<br />Name:' + text);
+        }
+    }, {
+        xtype: 'button',
+        text: '重置',
+        handler: function (sender) {
+            var select = sender.up('panel').select;
+            select.reset();
+            sender.up('panel').down('hidden[name=userIDs]').reset();
+            sender.up('panel').down('textfield[name=userNames]').reset();
+        }
+    }],
+
+    items: [{
+        xtype: 'textarea',
+        name: 'code1',
+        fieldLabel: '控件',
+        value: `var userSelect = Ext.create('App.widget.UserSelect', {
+            callback: function (records) {
+                    var ids = '';
+                    var names = '';
+                    for (var i = 0; i < records.length; i++) {
+                        var record = records[i];
+                        ids += record.data.ID + ',';
+                        names += record.data.Name + ',';
+                    }
+                    if (ids.endsWith(',')) {
+                        ids = ids.substr(0, ids.length -1);
+                    }
+                    if (names.endsWith(',')) {
+                        names = names.substr(0, names.length -1);
+                    }
+                    App.notify('消息', '您选择了<br />ID:' + ids + '<br />Name:' + names);
+                }
+            });
+            userSelect.reset();
+            userSelect.show();`
+    }, {
+        xtype: 'textarea',
+        name: 'code2',
+        fieldLabel: '获取值',
+        value: `var value = userSelect.getValue();
+            var text = userSelect.getRawValue();
+            App.notify('消息', '您选择了<br />ID:' + value + '<br />Name:' + text);`
+    }, {
+        xtype: 'textarea',
+        name: 'code3',
+        fieldLabel: '重置',
+        value: `userSelect.reset();`
+    }],
+
+    initComponent: function () {
+        this.callParent();
+        var view = this;
+        Ext.create('App.util.RequireCss').require(['packages/CodeMirror/lib/codemirror.css']);
+        require([
+            "packages/CodeMirror/lib/codemirror", "packages/CodeMirror/mode/htmlmixed/htmlmixed"
+        ], function (CodeMirror) {
+            var editor = CodeMirror.fromTextArea(view.down('textarea[name=code1]').inputEl.dom, {
+                lineNumbers: true,
+                mode: "javascript",
+                readOnly: true
+            });
+            editor.setSize('auto', '320px');
+            editor = CodeMirror.fromTextArea(view.down('textarea[name=code2]').inputEl.dom, {
+                lineNumbers: true,
+                mode: "javascript",
+                readOnly: true
+            });
+            editor.setSize('auto', '100px');
+            editor = CodeMirror.fromTextArea(view.down('textarea[name=code3]').inputEl.dom, {
+                lineNumbers: true,
+                mode: "javascript",
+                readOnly: true
+            });
+            editor.setSize('auto', '100px');
+        });
+    }
+});
---
+++ ExtApp/ExtApp.Web/app/widget/MapDraw.js
@@ -0,0 +1,32 @@
+﻿
+// ---------- 地图绘制控件 -----------
+
+Ext.define('App.widget.MapDraw', {
+    extend: 'Ext.window.Window',
+
+    type: 'point', // 绘制类型
+
+    title: '地图绘制',
+    width: 800,
+    height: 400,
+    closable: true,
+    maximizable: true,
+
+    buttons: [{
+        text: '确定',
+        handler: function () {
+
+        }
+    }, {
+        text: '取消',
+        handler: function (sender) {
+            sender.up('window').hide();
+        }
+    }],
+
+    listeners: {
+        afterrender: function (sender) {
+            sender.setHtml('<iframe src="packages/map/mapDraw.html?type=' + sender.type + '" width="100%" height="100%" frameborder="0" scrolling="auto"></iframe>');
+        }
+    }
+});

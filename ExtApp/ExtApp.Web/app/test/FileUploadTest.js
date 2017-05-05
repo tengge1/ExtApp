81a4a17de6ed825cf59f64e98e32fe2bd3cb0567
Li Teng <930372551@qq.com>
2017-05-05 20:49:36 +0800

    webuploader插件

---
+++ ExtApp/ExtApp.Web/app/test/FileUploadTest.js
@@ -0,0 +1,40 @@
+﻿
+Ext.define('App.test.FileUploadTest', {
+    extend: 'Ext.panel.Panel',
+
+    requires: [
+        'App.widget.FileUpload'
+    ],
+
+    layout: 'form',
+    autoScroll: true,
+
+    items: [{
+        xtype: 'fieldcontainer',
+        fieldLabel: '附件',
+        items: [{
+            xtype: 'fileupload'
+        }]
+    }, {
+        xtype: 'textarea',
+        name: 'code1',
+        fieldLabel: '控件',
+        value: ``
+    }],
+
+    initComponent: function () {
+        this.callParent();
+        var view = this;
+        Ext.create('App.util.RequireCss').require(['packages/CodeMirror/lib/codemirror.css']);
+        require([
+            "packages/CodeMirror/lib/codemirror", "packages/CodeMirror/mode/htmlmixed/htmlmixed"
+        ], function (CodeMirror) {
+            var editor = CodeMirror.fromTextArea(view.down('textarea[name=code1]').inputEl.dom, {
+                lineNumbers: true,
+                mode: "javascript",
+                readOnly: true
+            });
+            editor.setSize('auto', '250px');
+        });
+    }
+});
---
+++ ExtApp/ExtApp.Web/app/widget/FileUpload.js
@@ -0,0 +1,46 @@
+﻿
+// ---------- 附件上传控件 -----------
+
+Ext.define('App.widget.FileUpload', {
+    extend: 'Ext.grid.Panel',
+    alias: 'widget.fileupload',
+
+    width: 800,
+    height: 400,
+    layout: 'fit',
+
+    tbar: [{
+        xtype: 'filefield',
+        width: 60,
+        buttonOnly: true,
+        buttonText: '上传',
+        buttonConfig: {
+            iconCls: 'Add'
+        },
+        listeners: {
+            change: function (sender, value, opts) {
+                debugger;
+            }
+        }
+    }, {
+        xtype: 'button',
+        text: '删除',
+        iconCls: 'Delete'
+    }, {
+        xtype: 'button',
+        text: '查看',
+        iconCls: 'Photo'
+    }, {
+        xtype: 'button',
+        text: '下载',
+        iconCls: 'Delete'
+    }],
+
+    columns: [{
+        xtype: 'rownumberer'
+    }, {
+        text: '文件名'
+    }, {
+        text: '大小'
+    }]
+});

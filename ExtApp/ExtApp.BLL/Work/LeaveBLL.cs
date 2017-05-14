764aa396663ab5db877d380048a5f3ecf46c6bcb
Li Teng <930372551@qq.com>
2017-05-14 16:58:47 +0800

    消息dal、bll、controller

---
+++ ExtApp/ExtApp.BLL/Work/LeaveBLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Criterion;
+
+using ExtApp.Model;
+
+namespace ExtApp.BLL
+{
+    /// <summary>
+    /// 消息BLL
+    /// </summary>
+    public class LeaveBLL : BaseBLL<Leave>
+    {
+
+    }
+}
---
+++ ExtApp/ExtApp.Controllers/Work/LeaveController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Http.ModelBinding;
+
+using ExtApp.Model;
+using ExtApp.BLL;
+
+namespace ExtApp.Controller
+{
+    /// <summary>
+    /// 请假控制器
+    /// </summary>
+    public class LeaveController : ApiBase
+    {
+        /// <summary>
+        /// bll
+        /// </summary>
+        private LeaveBLL bll;
+    }
+}

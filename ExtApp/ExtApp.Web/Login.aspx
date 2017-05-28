6acdba1226448d711b6666e99dba208148a6fd0a
Li Teng <930372551@qq.com>
2017-05-28 12:42:09 +0800

    登录页面

---
+++ ExtApp/ExtApp.Web/Login.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="ExtApp.Web.Login" %>
+
+<!DOCTYPE html>
+
+<html lang="zh-cn">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>登录</title>
+    <link href="packages/extjs/icon.css" rel="stylesheet" />
+    <link href="css/common.css" rel="stylesheet" />
+    <script src="packages/extjs/ext-all-debug.js"></script>
+    <script src="packages/extjs/locale/locale-zh_CN.js"></script>
+    <script src="app/App.js"></script>
+    <script>
+        Ext.onReady(function () {
+            Ext.create('Ext.container.Viewport', {
+                layout: 'fit',
+                items: Ext.create('App.app.Login')
+            });
+        });
+    </script>
+</head>
+<body>
+</body>
+</html>
---
+++ ExtApp/ExtApp.Web/Login.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace ExtApp.Web
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                init();
+            }
+        }
+
+        private void init()
+        {
+            var path = ConfigurationManager.AppSettings["Theme"];
+            var control = new HtmlGenericControl();
+            control.TagName = "link";
+            control.Attributes.Add("rel", "stylesheet");
+            control.Attributes.Add("href", ResolveUrl(Page.ResolveClientUrl(path)));
+            Page.Header.Controls.AddAt(0, control);
+        }
+    }
+}
